f1c95e8bbc5d297499526d843b326a503ca56405
Mohammad alamin <alamin.cse.99@gmail.com>
2023-11-30 17:13:54 +0600

    completed

---
+++ client/src/Redux/State-Slice/taskSlice.js
@@ -0,0 +1,30 @@
+// import { createSlice } from '@reduxjs/toolkit'
+
+// export const taskSlice = createSlice({
+//   name: 'task',
+//   initialState: {
+//     New : [],
+//     Completed:[],
+//     Progress:[],
+//     Canceled:[]
+//   },
+//   reducers: {
+//     setNewTask:(state, action)=>{
+//         state.New=action.payload;
+//     },
+//     setCompleteTask:(state, action)=>{
+//         state.Completed=action.payload;
+//     },
+//     setProgressTask:(state, action)=>{
+//         state.Progress=action.payload;
+//     },
+//     setCanceledTask:(state, action)=>{
+//         state.Canceled=action.payload;
+//     },
+//   },
+// })
+
+
+// export const { setNewTask, setCompleteTask, setProgressTask, setCanceledTask } = taskSlice.actions
+
+// export default taskSlice.reducer
---
+++ client/src/Redux/Store/store.js
@@ -0,0 +1,7 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import taskReducer from '../State-Slice/taskSlice';
+// export default configureStore({
+//   reducer: {
+//     task:taskReducer,
+//   },
+// })
